30fdb2d0d6e231d63aed0279615ea0b59b00d5c9
Shahriyor Namozov <baxtiyarovich0102@gmail.com>
2025-07-31 16:26:15 +0500

    MongoDb connected

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express"
 import {create} from "express-handlebars"
+import mongoose from "mongoose"
+import * as dotenv from "dotenv"
+
 import AuthRoutes from "./routes/auth.js"
 import ProductsRoutes from "./routes/products.js"
 
+dotenv.config()
 
 const app =express()
 
@@ -14,11 +18,31 @@ const hbs = create({
 app.engine("hbs", hbs.engine)
 app.set('view engine', 'hbs');
 app.set('views', './views');
+
+app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
 
 app.use(AuthRoutes)
 app.use(ProductsRoutes)
 
-const PORT = process.env.PORT || 4000
-app.listen(4000, () => { console.log("Server is running", PORT);
-})
+
+
+const PORT = process.env.PORT || 4000;
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB ga ulandi");
+
+    app.listen(PORT, () => {
+      console.log("Server is running on", PORT);
+    });
+  } catch (err) {
+    console.error("MongoDB ga ulanishda xato:", err);
+    process.exit(1);
+  }
+};
+
+start();
