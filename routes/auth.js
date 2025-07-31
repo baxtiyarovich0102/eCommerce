5ad2a2bf3bb8a6e9f67fafb6256d3a1f90f76516
Shahriyor Namozov <baxtiyarovich0102@gmail.com>
2025-07-31 16:42:30 +0500

    data added

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 import {Router} from 'express'
+import User from '../models/User.js'
 
 const router = Router()
 
@@ -22,9 +23,14 @@ router.post("/login", (req, res) => {
     res.redirect('/')
 })
 
-router.post("/register", (req, res) => {
-    console.log((req.body));
-    
+router.post("/register", async (req, res) => {
+    const userData = {
+        firstName: req.body.firstname,
+        lastName: req.body.lastname,
+        email: req.body.email,
+        password: req.body.password
+    }
+    const user = await User.create(userData)
     res.redirect('/')
 })
 
