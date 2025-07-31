8a37baeba816cacea5c6482b0012dab5152e1cc7
Shahriyor Namozov <baxtiyarovich0102@gmail.com>
2025-07-31 11:18:09 +0500

    login and resgiter

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,29 @@ import {Router} from 'express'
 const router = Router()
 
 router.get("/login", (req, res) => {
-    res.render('login')
+    res.render('login', {
+        title: 'Login',
+        isLogin: true
+    })
 })
 
 router.get("/register", (req, res) => {
-    res.render('register')
+    res.render('register', {
+        title: 'Register',
+        isRegister: true
+    })
+})
+
+router.post("/login", (req, res) => {
+    console.log((req.body));
+    
+    res.redirect('/')
+})
+
+router.post("/register", (req, res) => {
+    console.log((req.body));
+    
+    res.redirect('/')
 })
 
 export default router
--- routes/products.js
+++ routes/products.js
@@ -3,15 +3,23 @@ import {Router} from 'express'
 const router = Router()
 
 router.get("/", (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'SHOP'
+    })
 })
 
 router.get("/products", (req, res) => {
-    res.render('products')
+    res.render('products', {
+        title: 'Products',
+        isProducts: true
+    })
 })
 
 router.get("/add", (req, res) => {
-    res.render('add')
+    res.render('add', {
+        title: 'Add Product',
+        isAdd: true
+    })
 })
 
 export default router
