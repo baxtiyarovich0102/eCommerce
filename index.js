7889abf5891da0ac536f66dae186caeebe7b3455
Shahriyor Namozov <baxtiyarovich0102@gmail.com>
2025-08-08 16:58:35 +0500

    connect-flash validator

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express"
 import {create} from "express-handlebars"
 import mongoose from "mongoose"
 import * as dotenv from "dotenv"
+import session from "express-session"
+import flash from "connect-flash"
 
 import AuthRoutes from "./routes/auth.js"
 import ProductsRoutes from "./routes/products.js"
@@ -22,6 +24,8 @@ app.set('views', './views');
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(session({secret: "SECRET", resave: false, saveUninitialized: false}))
+app.use(flash())
 
 
 app.use(AuthRoutes)
--- routes/auth.js
+++ routes/auth.js
@@ -7,29 +7,70 @@ const router = Router()
 router.get("/login", (req, res) => {
     res.render('login', {
         title: 'Login',
-        isLogin: true
+        isLogin: true,
+        loginError: req.flash('loginError')
     })
 })
 
 router.get("/register", (req, res) => {
     res.render('register', {
         title: 'Register',
-        isRegister: true
+        isRegister: true,
+        registerError: req.flash('registerError')
     })
 })
 
-router.post("/login", (req, res) => {
-    console.log((req.body));
+router.post("/login", async (req, res) => {
+    const {email, password} = req.body
+
+    if(!email || !password) {
+        req.flash('loginError', "All fields are required")
+        res.redirect('/login')
+        return
+    }
+
+    const existUser = await User.findOne({email})
+    if(!existUser) {
+        req.flash('loginError', "User not found")
+        res.redirect('/login')
+        return
+    }
+
+    const isPassEqual = await bcrypt.compare(password, existUser.password)
+
+    if(!isPassEqual) {
+        req.flash('loginError', "Password is wrong")
+        res.redirect('/login')
+        return
+    }
+    
+    console.log(existUser);
     
     res.redirect('/')
 })
 
 router.post("/register", async (req, res) => {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const {firstname, lastname, email, password} = req.body
+
+    if(!firstname || !lastname || !email || !password){
+        req.flash('registerError', "All fields are required")
+        res.redirect('/register')
+        return
+    }
+
+    const candidate = await User.findOne({email})
+
+    if(candidate) {
+        req.flash('registerError', "User already exist")
+        res.redirect('/register')
+        return
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
     const userData = {
-        firstName: req.body.firstname,
-        lastName: req.body.lastname,
-        email: req.body.email,
+        firstName: firstname,
+        lastName: lastname,
+        email: email,
         password: hashedPassword
     }
     const user = await User.create(userData)
